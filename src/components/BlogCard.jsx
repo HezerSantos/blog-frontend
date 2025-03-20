a0f5b4187b61b4f84e5891609fd636fb40960391
HezerSantos <HezerSantos24@gmail.com>
2025-03-20 00:10:30 -0500

    Finished basic frontend

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,21 +1,21 @@
 import dogImage from '../assets/images/dog.jpg';
-const BlogCard = () => {
+const BlogCard = ({image, header, text, author}) => {
     return(
         <>
             <div className="blog__card">
                 <div className="blog__image">
-                    <img src={dogImage} alt="" className='blog__image__content'/>
+                    <img src={image} alt="" className='blog__image__content'/>
                 </div>
                 <div className="blog__aside">
                     <h1 className="blog__header">
-                        First Experience With Mocha
+                        {header}
                     </h1>
                     <p className="blog__text">
-                    Becoming a first-time Pomeranian owner has been a mix of adorable moments and unexpected challenges. From learning about their high energy to navigating grooming routines, this post explores the reality of life with a fluffy ball of personality. I'll share my personal experiences, tips, and the lessons I’ve learned along the way, as well as how my Pomeranian has brought joy (and a little chaos) into my life. Whether you’re considering adopting one or just curious about these charming dogs, this post will give you an honest look at what it’s really like to welcome a Pomeranian into your home.
+                        {text}
                     </p>
                     <div className='blog__aside__footer'>
                         <button className="blog__open">Read More</button>
-                        <p className="blog__author">By: Jason Williams</p>
+                        <p className="blog__author">By: {author}</p>
                     </div>
                 </div>
             </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,10 @@ import MainNavBar from "../components/MainNavBar"
 import Footer from "../components/Footer"
 
 import BlogCard from "../components/BlogCard"
+
+import dogImage from '../assets/images/dog.jpg';
+import steak from '../assets/images/steak.jpg'
+import ball from '../assets/images/ball.jpg'
 const HomePage = () => {
     return(
         <>
@@ -19,7 +23,24 @@ const HomePage = () => {
                     <h1>TOP TRENDING</h1>
                 </div>
                 <section className="blog__container">
-                    <BlogCard />
+                    <BlogCard 
+                        image = {dogImage}
+                        header ="First Experience With Mocha"
+                        text = "Becoming a first-time Pomeranian owner has been a mix of adorable moments and unexpected challenges. From learning about their high energy to navigating grooming routines, this post explores the reality of life with a fluffy ball of personality. I'll share my personal experiences, tips, and the lessons I’ve learned along the way, as well as how my Pomeranian has brought joy (and a little chaos) into my life. Whether you’re considering adopting one or just curious about these charming dogs, this post will give you an honest look at what it’s really like to welcome a Pomeranian into your home."
+                        author = "Jason Williams"
+                    />
+                    <BlogCard 
+                        image = {steak}
+                        header ="The Secret to a Good Steak"
+                        text = "The secret to a perfect steak lies in a few key steps: choosing the right cut, seasoning, and mastering the cooking method. In this post, I’ll share the tips and techniques I’ve learned to bring out the best flavor and tenderness. From searing to resting, these simple tricks will help you elevate your steak game to restaurant-quality at home."
+                        author = "Jason Williams"
+                    />
+                    <BlogCard 
+                        image = {ball}
+                        header ="Is the NBA Failing?"
+                        text = "The NBA has long been a dominant force in global sports, but recent years have sparked debates about whether the league is facing a decline. From growing concerns about player empowerment, frequent injuries, and a lack of competitive balance, to the changing dynamics of viewership and fan engagement, this post takes a closer look at the NBA’s current challenges. I’ll explore whether these issues are a temporary setback or signs of deeper problems within the league. Is the NBA failing, or is it just evolving?"
+                        author = "Jason Williams"
+                    />
                 </section>
                 <div className="next__container">
                     <button>
