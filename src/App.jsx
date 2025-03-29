54afb365350df1f69bfc4f89fcc590eba6ec823d
HezerSantos <HezerSantos24@gmail.com>
2025-03-29 15:14:05 -0500

    Lolo

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import axios from "axios"
 import { Link, Outlet, Navigate } from "react-router-dom"
 import '../src/assets/styles/App.css'
 import AuthProvider from '../context/authContext'
+
+if (process.env.NODE_ENV === 'development') {
+    // Override all console methods
+    console.log = () => {};
+    console.warn = () => {};
+    console.error = () => {};
+    console.info = () => {};
+    console.debug = () => {};
+  }
+  
 function App() {
     axios.defaults.withCredentials = true;
     return (
